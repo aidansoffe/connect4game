e05c556277c865c23798421669550062e5a723da
aidansoffe <aidansoffe@gmail.com>
2020-09-01 21:08:05 -0700

    finished

--- connect4.js
+++ connect4.js
@@ -2,7 +2,7 @@
 
 var tableRow = document.getElementsByTagName('tr');
 var tableCell = document.getElementsByTagName('td');
-var tableSlot = document.querySelector('.slot');
+var tableSlot = document.querySelectorAll('.slot');
 const playerTurn = document.querySelector('.player-turn');
 const reset = document.querySelector('.reset')
 
@@ -16,12 +16,12 @@ for (let i = 0; i < tableCell.length; i++) {
 while(!player1) {
   var player1 = prompt('Player One: Enter your name. You will be red')
 }
-player1Color = 'red';
+player1Color = '#0197f6';
 
 while(!player2) {
   var player2 = prompt('Player Two: Enter your name. You will be blue')
 }
-player2Color = 'blue';
+player2Color = '#f1433f';
 
 var currentPlayer = 1;
 playerTurn.textContent = `${player1}'s turn!`
@@ -43,15 +43,33 @@ function changeColor(e) {
       if(currentPlayer === 1) {
         row[0].style.backgroundColor = player1Color;
         if(horizontalCheck() ||verticalCheck() || diagonalCheck1() || diagonalCheck2()){
-          return (alert('winner'));
+          playerTurn.textContent = `${player1} Wins!`
+          playerTurn.style.color = player1Color
+          return (alert(`${player1} Wins!`));
+        } else if(drawCheck()){
+          playerTurn.textContent = 'Game is draw!'
+          return alert("DRAW")
+        }else {
+          playerTurn.textContent = `${player2}'s turn!`;
+          return currentPlayer = 2
         }
-        playerTurn.textContent = `${player2}'s turn!`;
-        return currentPlayer = 2
+        
   }else {
     row[0].style.backgroundColor = player2Color;
     playerTurn.textContent = `${player1}'s turn!`;
-    return currentPlayer = 1
+    if(horizontalCheck() ||verticalCheck() || diagonalCheck1() || diagonalCheck2()){
+      playerTurn.textContent = `${player2} Wins!`
+      playerTurn.style.color = player2Color
+      return (alert(`${player2} Wins!`));
+    } else if(drawCheck()){
+      playerTurn.textContent = 'Game is draw!'
+      return alert("DRAW")
+    }else {
+      playerTurn.textContent = `${player1}'s turn!`;
+      return currentPlayer = 1
+    }
   }
+  
     }
   }
 }
@@ -88,7 +106,7 @@ function verticalCheck() {
 }
 
 function diagonalCheck1() {
-  for(let col = 0; col < 4; col++) {
+  for(let col = 0; col < 3; col++) {
     for(let row = 0; row < 3; row++ ){
       if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor,
         tableRow[row+1].children[col+1].style.backgroundColor,
@@ -113,3 +131,24 @@ function diagonalCheck2() {
     }
   }
 }
+
+function drawCheck() {
+  let fullSlot= [];
+  for( var i = 0; i < tableCell.length; i++) {
+    if(tableCell[i].style.backgroundColor !== 'white') {
+      fullSlot.push(tableCell[i])
+    }
+  }
+  if(fullSlot.length === tableCell.length) {
+    return true;
+  }
+}
+
+
+reset.addEventListener('click', ()=> {
+  tableSlot.forEach(slot =>{
+   return slot.style.backgroundColor = 'white';
+  });
+   playerTurn.style.color = 'black';
+  return (currentPlayer === 1 ? playerTurn.textContent = `${player1}'s turn` : playerTurn.textContent = `${player2}'s turn!`)
+})
